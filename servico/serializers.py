f4c8fdd6499b8cad17f94f23890f4d192edcd737
jefftb540 <jefferson.fl123@gmail.com>
2017-10-25 15:42:40 -0300

    Consertar erro na criptografia da senha

--- servico/serializers.py
+++ servico/serializers.py
@@ -2,7 +2,7 @@
 from rest_framework import  serializers
 from models import Notificacao, Categoria, SubCategoria, Usuario, Servico, Orcamento, Solicitacao, Mensagem, Avaliacao
 from drf_extra_fields.fields import Base64ImageField
-
+from django.contrib.auth.hashers import make_password
 
 class CategoriaSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -21,11 +21,18 @@ class UsuarioSerializer(serializers.ModelSerializer):
 		model = Usuario
 		fields = '__all__'
 
+	def update(self, instance, validated_data):
+		instance.password = make_password(validated_data.get('password', instance.password))
+		instance.save()
+		return instance
+
+
 class UsuarioSerializerNoPassword(serializers.ModelSerializer):
+
 	imagem= Base64ImageField()
 	class Meta:
 		model = Usuario
-		exclude = ('password',)
+		
 
 class UserCreateSerializer(serializers.ModelSerializer):
 	imagem= Base64ImageField()
